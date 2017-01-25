efbaa9ab4e9b5175ccb82e56b4984e98e7d008b3
Teoxishere <teodor.chiretu@gmail.com>
2017-01-25 21:55:20 +0200

    Matching Controller Done

---
+++ Controllers/MatchesController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using affnt.Models;
+
+namespace affnt.Controllers
+{
+    public class MatchesController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: Matches
+        public ActionResult Index()
+        {
+            Person per = db.People
+                        .Where(p => p.LookingFor == "F")
+                        .OrderBy(p => p.City)
+                        .FirstOrDefault();
+            return View(per);
+        }
+
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+
+
+
+    }
+       
+}
